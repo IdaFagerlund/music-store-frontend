6d33821f5760b7da9a64d0ee8c81a5af7f81ed06
Ida <ida.fagerlund@hotmail.com>
2020-05-18 10:10:24 +0200

    add react router and setup basic routing functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Test from "./test/ReduxTest"
+import User from "./test/User"
+import Admin from "./test/Admin"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Test />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/admin" component={Admin}></Route>
+        <Route path="/user" component={User}></Route>
+        <Route path="/" component={Test}></Route>
+      </Switch>
+    </Router>
   );
 }
 
