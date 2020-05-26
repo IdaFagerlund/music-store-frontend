0a94528a9044c83213d08da8c297f070e47662f9
Ida <ida.fagerlund@hotmail.com>
2020-05-26 21:44:38 +0200

    design for search results

--- src/components/pageheader/ProductSearch.jsx
+++ src/components/pageheader/ProductSearch.jsx
@@ -1,16 +1,31 @@
 import React, { useState, useEffect, useRef } from "react"
+import { useSelector } from "react-redux"
 import styles from "./ProductSearch.module.scss"
-import { MagnifyingGlassIcon } from "../utils/Icons"
+import { MagnifyingGlassIcon, ArrowRightIcon } from "../utils/Icons"
 import ContainerThatDisappearOnOutsideClick from "../utils/ContainerThatDisappearOnOutsideClick"
+import ElectricGuitarImage from "../../assets/electric_guitar.png"
 
 
 export default function ProductSearch() {
+    const products = useSelector((state) => state.products)
     const [searchText, setSearchText] = useState("")
     const parentContainerReference = useRef()
 
     const SearchResults = () => {
+        const searchResults = products.data.map(product =>
+            <div className={styles.SearchResult} key={product.id}>
+                <div className={styles.ImageAndNameContainer}>
+                    <img src={ElectricGuitarImage} width={64} height={64} alt="image" />
+                    <p>{product.name}</p>
+                </div>
+                <ArrowRightIcon size={20} color={"#808080"} />
+            </div>
+        )
+
         return (
-            <div className={styles.SearchResults}>hellooooo</div>
+            <div className={styles.SearchResults}>
+                {searchResults}
+            </div>
         )
     }
 
@@ -18,7 +33,8 @@ export default function ProductSearch() {
         <div className={styles.ProductSearch} ref={parentContainerReference}>
             <div className={`${styles.MagnifyingGlassIconContainer} ${searchText && styles.MagnifyingGlassIconContainerDuringSearch}`}><MagnifyingGlassIcon /></div>
             <input className={`${styles.SearchInput} ${searchText && styles.SearchInputDuringSearch}`} type="text" placeholder="Search..." onChange={event => setSearchText(event.target.value)} value={searchText} />
-            <ContainerThatDisappearOnOutsideClick containerContent={<SearchResults />} openContainerCondition={searchText} onContainerClose={() => setSearchText("")} parentContainerReference={parentContainerReference} />
+            <SearchResults />
+            {/* <ContainerThatDisappearOnOutsideClick containerContent={<SearchResults />} openContainerCondition={searchText} onContainerClose={() => setSearchText("")} parentContainerReference={parentContainerReference} /> */}
         </div>
     )
 }
