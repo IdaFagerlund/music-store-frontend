15c6f06c503a7e211410139a86df17bd285da2ba
Ida <ida.fagerlund@hotmail.com>
2020-06-04 08:39:02 +0200

    continue trying to figure out the production errors

--- src/redux/actions/api-service.js
+++ src/redux/actions/api-service.js
@@ -2,7 +2,7 @@ export async function fetchGet(url) {
     let customResponse = { status: null, headers: null, data: null }
     let wasRequestSuccessful = false
 
-    customResponse.data = await fetch(`${process.env.REACT_APP_SERVER_URL}${url}`, {
+    customResponse.data = await fetch(`https://jsonplaceholder.typicode.com/todos/1`, {
         method: "GET",
         credentials: "include",
         headers: {
@@ -31,7 +31,7 @@ export async function fetchPost(url, requestBody) {
     let customResponse = { status: null, headers: null, data: null }
     let wasRequestSuccessful = false
 
-    customResponse.data = await fetch(`${process.env.REACT_APP_SERVER_URL}${url}`, {
+    customResponse.data = await fetch(`https://jsonplaceholder.typicode.com/todos/1`, {
         method: "POST",
         credentials: "include",
         headers: {
