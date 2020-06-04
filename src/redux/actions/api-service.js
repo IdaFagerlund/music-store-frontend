ea7825bc96c697a3d7ce3b9a4aa98b4b41038ba2
Ida <ida.fagerlund@hotmail.com>
2020-06-04 08:53:17 +0200

    keep experiment with figuring out ssl errors

--- src/redux/actions/api-service.js
+++ src/redux/actions/api-service.js
@@ -2,7 +2,7 @@ export async function fetchGet(url) {
     let customResponse = { status: null, headers: null, data: null }
     let wasRequestSuccessful = false
 
-    customResponse.data = await fetch(`https://jsonplaceholder.typicode.com/todos/1`, {
+    customResponse.data = await fetch(`${process.env.REACT_APP_SERVER_URL}${url}`, {
         method: "GET",
         credentials: "include",
         headers: {
@@ -31,7 +31,7 @@ export async function fetchPost(url, requestBody) {
     let customResponse = { status: null, headers: null, data: null }
     let wasRequestSuccessful = false
 
-    customResponse.data = await fetch(`https://jsonplaceholder.typicode.com/todos/1`, {
+    customResponse.data = await fetch(`${process.env.REACT_APP_SERVER_URL}${url}`, {
         method: "POST",
         credentials: "include",
         headers: {
